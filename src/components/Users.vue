a970bd0085e6291960365429bf4263c69c869859
LucasBardoux <bardoux.l@gmx.de>
2019-03-23 11:46:10 +0100

    init vue project with material design

---
+++ src/components/Users.vue
@@ -0,0 +1,14 @@
+<template></template>
+
+<script>
+export default {
+  name: "users",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
