e4e67e0e1d019d904259625bc9fe0bc51f192c44
LucasBardoux <bardoux.l@gmx.de>
2019-03-23 15:28:47 +0100

    add table to User.vue and add id to db.json

--- src/components/User.vue
+++ src/components/User.vue
@@ -1,12 +1,45 @@
 <template>
-<div>User</div>
+  <div>
+    <md-table>
+      <md-table-row>
+        <md-table-head>Vorname</md-table-head>
+        <md-table-head>Nachname</md-table-head>
+        <md-table-head>Telefonnummer</md-table-head>
+        <md-table-head>E-Mail</md-table-head>
+      </md-table-row>
+      <md-table-row v-for="user in users" :key="user.id">
+        <md-table-cell>{{user.firstname}}</md-table-cell>
+        <md-table-cell>{{user.lastname}}</md-table-cell>
+        <md-table-cell>{{user.phone}}</md-table-cell>
+        <md-table-cell>{{user.mail}}</md-table-cell>
+      </md-table-row>
+    </md-table>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'user',
   data () {
-    return {}
+    return {
+      users: null
+    }
+  },
+  methods: {
+    init () {
+      axios
+        .get(
+          'https://my-json-server.typicode.com/LucasBardoux/user-dashboard/user'
+        )
+        .then(response => {
+          this.users = response.data
+        })
+    }
+  },
+  mounted () {
+    this.init()
   }
 }
 </script>
